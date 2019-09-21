d1610bda3d544fb65b6bb462e295d2bf34bfbdd1
Lama <apple@Lama.local>
2019-09-21 23:52:06 +0300

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -10,20 +10,41 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
+cardsInPlay.push(cards);
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay);
-
-var cardTwo = cards[2]
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-     alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
+
+}
+
+function flipCard(cardId){
+checkForMatch();
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+
+//var cardTwo = cards[2]
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
+
+//cards[cardId];
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards);
+
+//if (cardsInPlay.length === 2){
+//	if (cardsInPlay[0] === cardsInPlay[1]) {
+  //   alert("You found a match!");
+	//} else {
+	//	alert("Sorry, try again.");
+	//}
+//}
+}
+
+flipCard(0);
+flipCard(2);
 
